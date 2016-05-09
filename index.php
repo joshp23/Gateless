7b6694aafda1044c8cca7c86e020186bbe6d7511
Josh Panter <joshu@unfettered.net>
2016-05-09 12:29:03 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+require_once('gate.php');
+echo
+'<html>
+	<head>
+		<title>Unfettered</title>
+		<link rel="shortcut icon" href="/images/Mill.2.png" type="image/png" />
+	</head>
+	<body>
+		<h1 style="text-align: center;">Unfettered</h1>
+		<img  style="display:block;margin:0 auto;" src="/images/title_underline.gif"></br>
+		<h2 style="text-align: center;">Welcome</h2>
+		<p style="text-align: center;">This domain is currently under construction, and will be up and running soon enough. While things become ready, please stop by <a href="https://unfettered.net" target="_blank">our parent site</a>, instead.</p>
+		<p style="text-align: center;">Currently, there is no website...</p>
+		<img style="display:block;margin:15 auto;" src="/images/gateless.gif"></br></br>
+		<div style="white-space: pre-wrap;display:block;margin:0 auto;padding:15px;border:1px solid #021a40;box-shadow: 10px 10px 5px #888888;box-shadow: 10px 10px 5px #888888;background-color: lightgrey;">
+			<h3 style="text-align: center;text-decoration: underline;"><em>Mumonkan</em></h3>
+			<p>'.$gateless.'</p>
+			<img style="display:block;margin:0 auto;" src="/images/brush-stroke.png">
+		</div></br></br>
+		<a href="https://unfettered.net" target="_blank"><img style="display:block;margin:0 auto;" src="/images/Mill.2.png"></a></br>
+	</body>
+	<footer>
+	<!-- begin pronking -->
+		<a href="/nectarphial.php"></a>
+		<a href="https://code.unfettered.net/nectarphial.php"><!-- pronktification --></a>
+		<a href="https://code.unfettered.net/nectarphial.php" style="display: none;">pronktification</a>
+		<div style="display: none;"><a href="https://code.unfettered.net/nectarphial.php">pronktification</a></div>
+		<a href="https://code.unfettered.net/nectarphial.php"></a>
+		<!-- <a href="https://code.unfettered.net/nectarphial.php">pronktification</a> -->
+		<div style="position: absolute; top: -250px; left: -250px;"><a href="https://code.unfettered.net/nectarphial.php">pronktification</a></div>
+		<a href="https://code.unfettered.net/nectarphial.php"><span style="display: none;">pronktification</span></a>
+		<a href="https://code.unfettered.net/nectarphial.php"><div style="height: 0px; width: 0px;"></div></a>
+	<!-- end pronking -->
+	</footer>
+</html>'
+?>
